e1fa11d5dedb522729e2269b719595ce2eb8769f
Matthew Jeshua Martin <matthew-martin@programmer.net>
2024-01-15 11:21:11 -0500

    Database initial

--- src/main/java/com/binarymjm/carshow/CarShowApplication.java
+++ src/main/java/com/binarymjm/carshow/CarShowApplication.java
@@ -1,13 +1,21 @@
 package com.binarymjm.carshow;
 
+import com.binarymjm.carshow.domains.Car;
+import com.binarymjm.carshow.repository.CarRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CarShowApplication {
+import java.util.Arrays;
+import java.util.List;
 
+@SpringBootApplication
+public class CarShowApplication implements CommandLineRunner {
+    @Autowired
+    private CarRepository carRepository;
     private static final Logger logger = LoggerFactory.getLogger(CarShowApplication.class);
 
     public static void main(String[] args) {
@@ -15,4 +23,17 @@ public class CarShowApplication {
         logger.info("App Started");
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        List<Car> cars = Arrays.asList(
+                new Car("Ford", "Mustang", "MUS-32442", 2024,"Red", 0l, 75000.00),
+                new Car("Ford", "Mustang", "MUS-32442", 2024,"White", 0l, 75000.00),
+                new Car("Ford", "Mustang", "MUS-32442", 2024,"Black", 0l, 75000.00),
+                new Car("Ford", "F-150", "F150-32442", 2024,"Red", 0l, 75000.00),
+                new Car("Ford", "F-150", "F150-32442", 2024,"White", 0l, 75000.00),
+                new Car("Ford", "F-150", "F150-32442", 2024,"Black", 0l, 75000.00),
+                new Car("Ford", "F-150", "F150-32442", 2024,"Grey", 0l, 75000.00)
+        );
+        carRepository.saveAll(cars);
+    }
 }
--- src/main/java/com/binarymjm/carshow/domains/Car.java
+++ src/main/java/com/binarymjm/carshow/domains/Car.java
@@ -13,20 +13,30 @@ public class Car {
     private Long id;
     private String make;
     private String model;
+    private String registerNumber;
     private int year;
     private String color;
-    private Long owner_id;
+    private Long ownerId;
+    private double price;
 
     public Car() {
-
     }
 
-    public Car(String make, String model, int year, String color, Long owner_id) {
+    public Car(
+               String make,
+               String model,
+               String registerNumber,
+               int year,
+               String color,
+               Long ownerId,
+               double price) {
         this.make = make;
         this.model = model;
+        this.registerNumber = registerNumber;
         this.year = year;
         this.color = color;
-        this.owner_id = owner_id;
+        this.ownerId = ownerId;
+        this.price = price;
     }
 
     public Long getId() {
@@ -69,12 +79,29 @@ public class Car {
         this.color = color;
     }
 
-    public Long getOwner_id() {
-        return owner_id;
+
+    public String getRegisterNumber() {
+        return registerNumber;
     }
 
-    public void setOwner_id(Long owner_id) {
-        this.owner_id = owner_id;
+    public void setRegisterNumber(String registerNumber) {
+        this.registerNumber = registerNumber;
+    }
+
+    public Long getOwnerId() {
+        return ownerId;
+    }
+
+    public void setOwnerId(Long ownerId) {
+        this.ownerId = ownerId;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
     }
 
     @Override
@@ -83,9 +110,11 @@ public class Car {
                 "id=" + id +
                 ", make='" + make + '\'' +
                 ", model='" + model + '\'' +
+                ", registerNumber='" + registerNumber + '\'' +
                 ", year=" + year +
                 ", color='" + color + '\'' +
-                ", owner_id=" + owner_id +
+                ", ownerId=" + ownerId +
+                ", price=" + price +
                 '}';
     }
 }
